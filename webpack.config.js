d4fde808fbb7149d10e2883cdedd58765d832993
SaiSreekar9666 <gsaisreekar9666@gmail.com>
2025-06-11 01:38:41 +0530

    changes2

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,36 @@
 //     ],
 //   },
 // };
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   entry: './scripts/index.js', // Your entry file
+//   output: {
+//     filename: 'bundle.js', // The output bundle file
+//     path: path.resolve(__dirname, 'dist'), // Output directory
+//     clean: true, // Cleans the output directory before building
+//   },
+//   mode: 'production', // Build mode
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './index.html', // Path to your HTML template
+//       filename: 'index.html', // Output HTML file name
+//     }),
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'], // Loaders for CSS files
+//       },
+//       {
+//         test: /\.(png|jpe?g|gif|avif)$/i, // Loaders for images
+//         type: 'asset/resource',
+//       },
+//     ],
+//   },
+// };
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -23,10 +53,10 @@ module.exports = {
   entry: './scripts/index.js', // Your entry file
   output: {
     filename: 'bundle.js', // The output bundle file
-    path: path.resolve(__dirname, 'dist',public), // Output directory
+    path: path.resolve(__dirname, 'dist'), // Output directory (dist instead of public)
     clean: true, // Cleans the output directory before building
   },
-  mode: 'production', // Build mode
+  mode: 'production', // Build mode (production for optimized output)
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html', // Path to your HTML template
